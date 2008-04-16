4594e18cda69c9061b1b6b0399bb8594901cbd33
Brian Clapper <bmc@clapper.org>
2008-04-16 02:29:27 +0000

    1. Added grizzled.file.copyRecursively() function. 2. Added Wing project file.

--- grizzled/file/__init__.py
+++ grizzled/file/__init__.py
@@ -53,6 +53,51 @@ def recursivelyRemove(dir):
                                    (f, dir, sys.exc_info()[1])
         os.rmdir(dir)
 
+def copyRecursively(sourceDir, targetDir):
+    """
+    Recursively copy a source directory (and all its contents) to a target
+    directory.
+    
+    @type sourceDir:  str
+    @param sourceDir: Source directory to copy recursively. This path must
+                      exist and must specify a directory; otherwise, this
+                      function throws a C{ValueError}
+    
+    @type targetDir:  str
+    @param targetDir: Directory to which to copy the contents of C{sourceDir}.
+                      This directory is created if it does not exist.
+                      
+    @rtype:  int
+    @return: the number of files copied (but not the number of directories
+             created)
+             
+    @raise ValueError: If: C{sourceDir} does not exist; C{sourceDir} exists
+                       but is not a directory; or C{targetDir} exists but is
+                       not a directory.
+    """
+    if not os.path.exists(sourceDir):
+        raise ValueError, 'Source directory "%s" does not exist' % sourceDir
+
+    if not os.path.isdir(sourceDir):
+        raise ValueError, \
+              'Source directory "%s" is not a directory' % sourceDir
+
+    if os.path.exists(targetDir) and (not os.path.isdir(targetDir)):
+        raise ValueError, \
+              'Target "%s" exists and is not a directory' % targetDir
+
+    total = 0
+    for dir, subdirs, files in os.walk(sourceDir, topdown=False):
+        d = dir[len(sourceDir) + 1:]
+        targetSubdir = os.path.join(targetDir, d)
+        if not os.path.exists(targetSubdir):
+            os.makedirs(targetSubdir)
+
+        copy([os.path.join(dir, file) for file in files], targetSubdir)
+        total += len(files)
+
+    return total
+
 def copy(files, targetDir, createTarget=False):
     """
     Copy one or more files to a target directory.
