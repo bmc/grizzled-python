273441d9a9260b940e1eccc809714ce33319e897
Brian Clapper <bmc@clapper.org>
2008-05-31 04:09:55 +0000

    another doc fix

--- grizzled/__init__.py
+++ grizzled/__init__.py
@@ -1,7 +1,7 @@
 # $Id$
 
 """
-The I{Grizzled Utility Library} is a general-purpose Python library with
+The *Grizzled Utility Library* is a general-purpose Python library with
 a variety of different modules and packages. It's roughly organized into
 subpackages that group different kinds of utility functions and classes.
 """
