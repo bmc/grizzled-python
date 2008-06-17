26c1ced77dcea375727b78186762c87bcfba76de
Brian Clapper <bmc@clapper.org>
2008-06-17 14:06:18 +0000

    Added grizzled.misc module and ReadOnly class.

---
+++ grizzled/misc.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+#
+# $Id$
+
+# NOTE: Documentation is intended to be processed by epydoc and contains
+# epydoc markup.
+
+"""
+Overview
+========
+
+The ``grizzled.misc`` module contains miscellanous functions and classes that
+don't seem to fit well in other modules.
+"""
+
+# ---------------------------------------------------------------------------
+# Imports
+# ---------------------------------------------------------------------------
+
+import logging
+
+from grizzled.exception import ExceptionWithMessage
+
+# ---------------------------------------------------------------------------
+# Exports
+# ---------------------------------------------------------------------------
+
+__all__ = ['ReadOnly', 'ReadOnlyObjectError']
+
+# ---------------------------------------------------------------------------
+# Constants
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+# Logging
+# ---------------------------------------------------------------------------
+
+log = logging.getLogger('grizzled.misc')
+
+# ---------------------------------------------------------------------------
+# Public classes
+# ---------------------------------------------------------------------------
+
+class ReadOnlyObjectError(ExceptionWithMessage):
+    """
+    Thrown by ``ReadOnly`` to indicate an attempt to set a field.
+
+    :IVariables:
+        field_name : str
+            name of the read-only field that could not be set
+
+        message : str
+            message to associated with the exception
+    """
+    def __init__(self, field_name, message):
+        ExceptionWithMessage.__init__(self, message)
+        self.field_name = field_name
+
+class ReadOnly(object):
+    """
+    A ``ReadOnly`` object wraps another object and prevents all the contained
+    object's fields from being written. Example use:
+    
+    .. python::
+    
+        from invitemedia.lang import ReadOnly
+
+        config = IMConfigParser()
+        config.read('/path/to/some/file')
+        roConfig = ReadOnly(config)
+        
+    Any attempt to set fields within ``roConfig`` will cause a 
+    ``ReadOnlyObjectError`` to be raised.
+    
+    The ``__class__`` member of the instantiate ``ReadOnly`` class will be the
+    class of the contained object, rather than ``ReadOnly``
+    (``IMConfigParser`` in the example). Similarly, the ``isinstance()``
+    built-in function will compare against the contained object's class.
+    However, the ``type()`` built-in will return the ``ReadOnly`` class
+    object.
+    """
+    def __init__(self, wrapped):
+        """
+        Create a new ``ReadOnly`` object that wraps the ``wrapped`` object
+        and enforces read-only access to it.
+        
+        :Parameters:
+            wrapped : object
+                the object to wrap
+        """
+        self.wrapped = wrapped
+
+    def __getattribute__(self, thing):
+        wrapped = object.__getattribute__(self, 'wrapped')
+        result = None
+        if thing == 'wrapped':
+            result = wrapped
+        else:
+            result = getattr(wrapped, thing)
+
+        return result
+
+    def __setattr__(self, thing, value):
+        if thing == 'wrapped':
+            object.__setattr__(self, thing, value)
+        else:
+            raise ReadOnlyObjectError(thing, 
+                                      'Attempt to access field "%s" of '
+                                      'read-only %s object' %
+                                      (thing, self.wrapped.__class__.__name__))
+
+# ---------------------------------------------------------------------------
+# Public functions
+# ---------------------------------------------------------------------------
